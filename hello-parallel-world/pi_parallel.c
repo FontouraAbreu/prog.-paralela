81128de89c98facfa5a6a9b7e12bcdc2ebc46873
Vinícius Fontoura <vfa20@inf.ufpr.br>
2025-04-22 18:20:40 -0300

    [update]

---
+++ hello-parallel-world/pi_parallel.c
@@ -0,0 +1,33 @@
+#include <omp.h>
+#include <stdio.h>
+
+# define NUM_THREADS 8
+
+
+static long num_steps= 1000000000;
+double step;
+
+
+int main () {
+    int i;
+    double pi;
+
+    double sum = 0.0;
+    step = 1.0/(double) num_steps;
+    
+    #pragma omp parallel
+    {
+        #pragma omp for reduction (+:sum) schedule(auto)
+        {
+            double x = 0.0;
+            for (i=0;i< num_steps; i++){
+                x = (i + 0.5) * step; // Largura do retângulo
+                sum = sum + 4.0 / (1.0 + x*x); // Sum += Área do retângulo
+            }
+        } 
+    }
+    pi = step * sum;
+
+    printf("pi is: %.20f\n", pi);
+
+}
