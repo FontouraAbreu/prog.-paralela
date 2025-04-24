99ba25b40018825bf60907381357efe697beef9a
Vinícius Fontoura <vfa20@inf.ufpr.br>
2025-04-24 18:15:19 -0300

    [change] private(x)

--- hello-parallel-world/pi_parallel.c
+++ hello-parallel-world/pi_parallel.c
@@ -11,15 +11,15 @@ double step;
 int main () {
     int i;
     double pi;
-
+    
+    double x = 0.0;
     double sum = 0.0;
     step = 1.0/(double) num_steps;
     
     #pragma omp parallel
     {
-        #pragma omp for reduction (+:sum) schedule(auto)
+        #pragma omp for reduction private(x) (+:sum) schedule(auto)
         {
-            double x = 0.0;
             for (i=0;i< num_steps; i++){
                 x = (i + 0.5) * step; // Largura do retângulo
                 sum = sum + 4.0 / (1.0 + x*x); // Sum += Área do retângulo
