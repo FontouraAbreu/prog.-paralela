3d90aaf996fd61ba918264b34c53dae660040ba9
vinicius <vinicius.fontoura@pop-pr.rnp.br>
2025-06-22 13:49:10 -0300

    [wip]

--- T2/slurm_script.sh
+++ T2/slurm_script.sh
@@ -7,7 +7,6 @@
 #SBATCH --time=4:00:00
 #SBATCH --cpu-freq=high
 
-# INPUT_SIZES=("1000" "2000" "3000" "4000" "5000" "6000" "7000" "8000" "9000" "10000" "11000" "12000" "13000" "14000" "15000" "16000" "17000" "18000" "19000" "20000")
 INPUT_SIZES=("10000" "20000" "30000" "40000" "50000" "60000" "70000" "80000" "90000" "100000" "110000" "120000" "130000" "140000" "150000")
 NPROCS_LIST=(2 4 8 12)
 REPS=20
@@ -19,10 +18,14 @@ for SIZE in "${INPUT_SIZES[@]}"; do
 
     SEQ_RESULTS=()
 
-    # Inicializa array de tempos para cada configuração paralela
-    declare -A PAR_TIMES
+    declare -A PAR_TOTAL_TIMES
+    declare -A PAR_CALC_TIMES
+    declare -A PAR_COMM_TIMES
+
     for NP in "${NPROCS_LIST[@]}"; do
-        PAR_TIMES[$NP]=""
+        PAR_TOTAL_TIMES[$NP]=""
+        PAR_CALC_TIMES[$NP]=""
+        PAR_COMM_TIMES[$NP]=""
     done
 
     for REP in $(seq 1 $REPS); do
@@ -32,7 +35,6 @@ for SIZE in "${INPUT_SIZES[@]}"; do
 
         SEQ_TIME=$(./seq "$FILE_A" "$FILE_B" | grep "Time:" | awk '{print $2}')
         echo "SeqTime: $SEQ_TIME"
-
         SEQ_RESULTS+=("$SEQ_TIME")
 
         for NP in "${NPROCS_LIST[@]}"; do
@@ -40,39 +42,62 @@ for SIZE in "${INPUT_SIZES[@]}"; do
             echo "Run $REP - Input Size: $SIZE - MPI with $NP processes"
             echo "--------------------------------------------"
 
-            PAR_TIME=$(mpirun -np $NP ./par "$FILE_A" "$FILE_B" | grep "Time:" | awk '{print $2}')
-            echo "ParTime (NP=$NP): $PAR_TIME"
+            OUTPUT=$(mpirun -np $NP ./par "$FILE_A" "$FILE_B")
 
-            PAR_TIMES[$NP]="${PAR_TIMES[$NP]} $PAR_TIME"
+            TOTAL_TIME=$(echo "$OUTPUT" | grep "TotalTime:" | awk '{print $2}')
+            CALC_TIME=$(echo "$OUTPUT" | grep "CalcTime:" | awk '{print $2}')
+            COMM_TIME=$(echo "$OUTPUT" | grep "CommTime:" | awk '{print $2}')
+
+            echo "Total: $TOTAL_TIME | Calc: $CALC_TIME | Comm: $COMM_TIME"
+
+            PAR_TOTAL_TIMES[$NP]="${PAR_TOTAL_TIMES[$NP]} $TOTAL_TIME"
+            PAR_CALC_TIMES[$NP]="${PAR_CALC_TIMES[$NP]} $CALC_TIME"
+            PAR_COMM_TIMES[$NP]="${PAR_COMM_TIMES[$NP]} $COMM_TIME"
         done
     done
 
-    # Gerar resumo de sequencial
+    # ==== Salvar resumo sequencial ====
     SUMMARY_SEQ="summary_seq_${SIZE}.txt"
     echo "Gerando resumo sequencial: $SUMMARY_SEQ"
-
     echo "Threads: 1" > "$SUMMARY_SEQ"
+
     AVG=$(echo "${SEQ_RESULTS[@]}" | awk '{for(i=1;i<=NF;i++) sum+=$i; print sum/NF}')
     STD=$(echo "${SEQ_RESULTS[@]}" | awk -v avg="$AVG" '{for(i=1;i<=NF;i++) sum+=($i-avg)^2; print sqrt(sum/NF)}')
     printf "AVG Time: %.6f seconds\n" "$AVG" >> "$SUMMARY_SEQ"
     printf "Deviation: %.6f\n\n" "$STD" >> "$SUMMARY_SEQ"
 
-    # Gerar resumo paralelo
+    # ==== Salvar resumo paralelo ====
     SUMMARY_PAR="summary_par_${SIZE}.txt"
     echo "Gerando resumo paralelo: $SUMMARY_PAR"
     echo "" > "$SUMMARY_PAR"
 
     for NP in "${NPROCS_LIST[@]}"; do
-        TIMES="${PAR_TIMES[$NP]}"
-        AVG=$(echo $TIMES | awk '{for(i=1;i<=NF;i++) sum+=$i; print sum/NF}')
-        STD=$(echo $TIMES | awk -v avg="$AVG" '{for(i=1;i<=NF;i++) sum+=($i-avg)^2; print sqrt(sum/NF)}')
+        TOTALS="${PAR_TOTAL_TIMES[$NP]}"
+        CALCS="${PAR_CALC_TIMES[$NP]}"
+        COMMS="${PAR_COMM_TIMES[$NP]}"
+
+        # Média e desvio padrão TotalTime
+        AVG_TOTAL=$(echo $TOTALS | awk '{for(i=1;i<=NF;i++) sum+=$i; print sum/NF}')
+        STD_TOTAL=$(echo $TOTALS | awk -v avg="$AVG_TOTAL" '{for(i=1;i<=NF;i++) sum+=($i-avg)^2; print sqrt(sum/NF)}')
+
+        # Média e desvio padrão CalcTime
+        AVG_CALC=$(echo $CALCS | awk '{for(i=1;i<=NF;i++) sum+=$i; print sum/NF}')
+        STD_CALC=$(echo $CALCS | awk -v avg="$AVG_CALC" '{for(i=1;i<=NF;i++) sum+=($i-avg)^2; print sqrt(sum/NF)}')
+
+        # Média e desvio padrão CommTime
+        AVG_COMM=$(echo $COMMS | awk '{for(i=1;i<=NF;i++) sum+=$i; print sum/NF}')
+        STD_COMM=$(echo $COMMS | awk -v avg="$AVG_COMM" '{for(i=1;i<=NF;i++) sum+=($i-avg)^2; print sqrt(sum/NF)}')
 
         echo "Threads: $NP" >> "$SUMMARY_PAR"
-        printf "AVG Time: %.6f seconds\n" "$AVG" >> "$SUMMARY_PAR"
-        printf "Deviation: %.6f\n\n" "$STD" >> "$SUMMARY_PAR"
+        printf "AVG Time: %.6f seconds\n" "$AVG_TOTAL" >> "$SUMMARY_PAR"
+        printf "AVG Calc: %.6f seconds\n" "$AVG_CALC" >> "$SUMMARY_PAR"
+        printf "AVG COMM: %.6f seconds\n" "$AVG_COMM" >> "$SUMMARY_PAR"
+        printf "Deviation: %.6f\n\n" "$STD_TOTAL" >> "$SUMMARY_PAR"
     done
 
-    # Limpa arrays antes de começar o próximo tamanho
+    # Limpa arrays antes do próximo tamanho
     unset SEQ_RESULTS
-    unset PAR_TIMES
+    unset PAR_TOTAL_TIMES
+    unset PAR_CALC_TIMES
+    unset PAR_COMM_TIMES
 done
